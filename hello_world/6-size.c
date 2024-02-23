4501e69bd12a24d9d0bd292f6b6de3088239ffdf
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-23 19:52:17 +0000

    Task6

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stido.h>
+/**
+ *
+ *main - Entrance point of the program
+ *
+ */
+
+int main()
+{
+  printf(sizeof(char));
+  printf(sizeof(int));
+  printf(sizeof(float));
+  printf(sizeof(double));
+  printf(sizeof(long));
+  printf(sizeof(long long));
+
+  return (0);
+}
