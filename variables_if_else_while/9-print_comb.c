b4d904e73e320edcd9aa5eaafe14d61e16e689ef
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-23 23:26:08 +0000

    Task9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,10 +11,11 @@ int ten = 10;
 int nine = 9;
 for (number = 0; number < ten; number++)
 {
-putchar( number + '0');
+putchar(number + '0');
 if (number != nine)
 {
 putchar(',');
+putchar(' ');
 }
 }
 putchar('\n');
