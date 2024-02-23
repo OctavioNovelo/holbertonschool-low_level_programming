57676ea42c9b416bc2d8a429572790249eb3bc5a
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-23 19:41:55 +0000

    Task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - Entr point of the program
+ *
+ */
+
 int main()
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
+  printf(" with proper grammar, but the outcome is a piece of art,");
 
   return (0);
 }
