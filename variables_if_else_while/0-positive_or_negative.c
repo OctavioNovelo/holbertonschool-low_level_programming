1ad922b655e253a780f6f1584ecbed5bda430ae8
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-23 20:59:35 +0000

    Task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,8 @@
 int main(void)
 {
 int n;
-
 srand(time(0)); 
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -23,6 +21,5 @@ else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-
 return (0);
 }
