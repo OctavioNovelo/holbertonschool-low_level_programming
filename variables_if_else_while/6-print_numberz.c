e145e8a95073dc3b474e8e513a60643dad1afc66
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-23 22:53:05 +0000

    Task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int number;
+int ten = 10;
+for (number = 0; number < ten; number++)
+{
+printf("%d", number);
+}
+printf("\n");
+return (0);
+}
