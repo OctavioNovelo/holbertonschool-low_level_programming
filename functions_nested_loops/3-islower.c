4130b90ce8434cc7999ad6ed5d4716576789abd9
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-25 03:01:41 +0000

    Task3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main."
+/**
+ *
+ * int _islower(int c) - Check if the char is lowercase
+ *
+ * Return - Always 0 (Sucess)
+ *
+ */
+int _islower(int c)
+{
+int c;
+
+if (c >= 'a' && c <= 'z')
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+}
