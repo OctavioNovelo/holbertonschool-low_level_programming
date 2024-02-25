0e21894a68d8a376f0c8b8f8421c35fe6b410732
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-25 04:09:19 +0000

    Task7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 /**
  * print_last_digit - prints the last digit of a number
- * 
+ *
  * @n: the number to find the last digit
  *
  * Return: the value of the last digit
@@ -19,10 +19,10 @@ else
 {
 if (n < 0)
 {
-n = -n; 
+n = -n;
 }
 last_digit = n % 10;
 }
 _putchar('0' + last_digit);
-return last_digit;
+return (last_digit);
 }
