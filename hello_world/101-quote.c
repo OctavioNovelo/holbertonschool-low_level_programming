3156d4296db3cc6288c14783d1d590b63bfda825
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-25 05:14:12 +0000

    Task9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2, message, 59);
+printf('\n');
 return (1);
 }
