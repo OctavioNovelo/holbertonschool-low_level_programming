1c0d99001e3be5314fd7741e02980eb584ead038
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-28 23:58:54 +0000

    Task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,31 +2,20 @@
 #include <stdio.h>
 int _isupper(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-printf("1");
-}
-else
-{
-printf("1");
-}
-return (0);
-}
-
-int main()
-{
 char c;
  int i;
+ int up = 1;
+ int low = 0;
  c = 'A';
  for (i = 0; i <= 90; i++)
    {
-printf("%c: %d\n", c, _isupper(c));
+printf("%c: %d\n", c, up;
  c++;
    }
  c = 'a';
  for (i = 0; i < 97; i++)
    {
-printf("%c: %d\n", c, _isupper(c));
+printf("%c: %d\n", c, low;
  c++;
    }
  return (0);
