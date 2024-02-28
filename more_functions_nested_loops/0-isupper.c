5451f354eb760e7dfe223ec096e8a356d321d796
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-28 23:39:21 +0000

    Task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@ int _isupper(int c)
 {
   if (c >= 'a' && c <= 'z')
     {
-      _putchar("1");
+      printf("1");
     }
   else
     {
-      _putchar("1");
+      printf("1");
     }
   _putchar('\n');
   return (0);
