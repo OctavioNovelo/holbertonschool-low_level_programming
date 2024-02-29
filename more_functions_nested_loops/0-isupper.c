7455de3b6a6cdd9abc89e7738bc1a763cb19dd22
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-29 00:01:59 +0000

    Task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,20 +3,20 @@
 int _isupper(int c)
 {
 char a;
- int i;
- int up = 1;
- int low = 0;
- a = 'A';
- for (i = 0; i <= 90; i++)
-   {
-printf("%c: %d\n", c, up;
- a++;
-   }
- a = 'a';
- for (i = 0; i < 97; i++)
-   {
-printf("%c: %d\n", c, low;
- a++;
-   }
- return (0);
+int i;
+int up = 1;
+int low = 0;
+a = 'A';
+for (i = 0; i <= 90; i++)
+{
+printf("%c: %d\n", c, up);
+a++;
+}
+a = 'a';
+for (i = 0; i < 97; i++)
+{
+printf("%c: %d\n", c, low);
+a++;
+}
+return (0);
 }
