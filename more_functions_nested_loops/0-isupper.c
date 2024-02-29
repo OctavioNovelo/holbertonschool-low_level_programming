fb7158c18af4e66fa784b8fc2509216a3fb17da3
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-29 00:05:09 +0000

    Task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,13 @@ int low = 0;
 a = 65;
 for (i = 0; i <= 90; i++)
 {
-printf("%c: %d\n", c, up);
+printf("%c: %d\n", a, up);
 a++;
 }
 a = 97;
 for (i = 0; i < 97; i++)
 {
-printf("%c: %d\n", c, low);
+printf("%c: %d\n", a, low);
 a++;
 }
 return (0);
