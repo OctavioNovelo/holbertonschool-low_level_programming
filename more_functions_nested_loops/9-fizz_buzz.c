731ca9ef381f2ad4bd795ff37e030fa620cdba49
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-01 03:44:54 +0000

    Task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int main()
 int i;
 for (i = 0; i <= 100; i++)
 {
-if ((i % 3) == 0 && (i % 5) == 0)
+if ((i / 3) == 0 && (i / 5) == 0)
 {
 printf("FizzBuzz ");
 }
-if ((i % 3) == 0)
+else if ((i / 3) == 0)
 {
 printf("Fizz ");
 }
-if ((i % 5) == 0)
+else if ((i / 5) == 0)
 {
 printf("Buzz ");
 }
