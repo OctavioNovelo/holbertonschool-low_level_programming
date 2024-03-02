1d1be99f694ff78e7837f9c4985ef67a7267e89b
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-02 16:16:03 +0000

    Task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - copy the string for src tod dest
+ *
+ *@dest: pointer 1
+ *@src: pointer 2
+ *
+ *Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
