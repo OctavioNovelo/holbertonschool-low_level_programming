ff40f57de9b85cb0440e8e25aa066b88ee732171
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-02 16:56:47 +0000

    Task10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+if (*s == '\0')
+{
+return(0);
+}
+while (*s == ' ')
+{
+s++;
+}
+if (*s == '-' || *s == '+')
+{
+sign = (*s == '-') ? -1 : 1;
+s++;
+}
+while (*s >= '0' && *s <= '9')
+{
+if (result > (INT_MAX - (*s - '0')) / 10)
+{
+if (sign == 1)
+{
+return INT_MAX;
+}
+else
+{
+return (INT_MIN);
+}
+}
+result = result * 10 + (*s - '0');
+s++;
+}
+return (result * sign);
+}
