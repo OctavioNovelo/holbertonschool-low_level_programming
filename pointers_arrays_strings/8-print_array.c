f4b7a4203dbd38d9d77976936499430b8a4eb85c
OctavioNovelo <octavionovelo22@gmai.com>
2024-03-02 02:36:48 +0000

    Task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - print an a array
+ *
+ *@n: number of elements of the array to be printed
+ *
+ *@a: pointer
+ */
+void print_array(int *a, int n)
+{
+while (n <= 0)
+{
+printf("%d, ", *a);
+}
+}
