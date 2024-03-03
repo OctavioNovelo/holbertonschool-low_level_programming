7d541efa895847c2bbfebf2617d30531d420cfe7
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-03 04:50:12 +0000

    Task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,19 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * puts_half - Prints half of a string, followed by a new line.
  * @str: The input string.
  */
 void puts_half(char *str)
 {
-int i;
-int length = 0;
-int start_index = (length - 1) / 2 + 1;
-if (str != NULL)
-{
+int length = 0, i, start;
 while (str[length] != '\0')
 {
 length++;
 }
-start_index = length / 2;
-for (i = start_index; i < length; i++)
+start = (length + 1) / 2;
+for (i = start; i < length; i++)
 {
 _putchar(str[i]);
 }
 _putchar('\n');
-}
 }
