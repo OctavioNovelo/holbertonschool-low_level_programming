5f744dd438af5cbfa613c5cbb68d5d726fb9402f
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-03 05:20:59 +0000

    Task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,27 +8,19 @@
 char *cap_string(char *str)
 {
 int i = 0;
-while (str[i] != '\0')
-{
 if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - ('a' - 'A');
 }
+while (str[i] != '\0')
+{
 if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 str[i] == '!' || str[i] == '?' || str[i] == '"' ||
 str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}' || str[i] == '_')
+str[i] == '}')
 {
 i++;
-while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}' || str[i] == '_')
-{
-i++;
-}
 if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - ('a' - 'A');
