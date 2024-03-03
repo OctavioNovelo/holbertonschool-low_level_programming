af5e4c5a6e78ca2f2c957ab0dbcc0f25110bd364
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-03 05:25:18 +0000

    Task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The input string.
@@ -8,33 +7,30 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-
-    if (str[i] >= 'a' && str[i] <= 'z')
-    {
-        str[i] = str[i] - ('a' - 'A');
-    }
-
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
-            i++;
-            while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-            {
-                i++;
-            }
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] = str[i] - ('a' - 'A');
-            }
-        }
-        i++;
-    }
-
-    return (str);
+int i = 0;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+while (str[i] != '\0')
+{
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}')
+{
+i++;
+while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+{
+i++;
+}
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+}
+i++;
+}
+return (str);
 }
