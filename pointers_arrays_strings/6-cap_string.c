6287c82cfa5ecb3a2bb6cb69de36924c73c2f44e
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-03 00:27:43 +0000

    Task6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+while (str[i] != '\0')
+{
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}')
+{
+i++;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+}
+i++;
+}
+return (str);
+}
