b6cbca4824b2810146b9ffc4bb5a98d8335e3a6a
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-03 05:17:55 +0000

    Task11

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Generates random valid passwords for 101-crackme.
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-    if (argc != 2)
-    {
-        printf("Usage: %s password\n", argv[0]);
-        return 1; 
-    }
-
-    int i, length, j;
-    char password[12];
-    srand(time(NULL));
-
-    for (i = 0; i < 10; i++)
-    {
-        length = rand() % 7 + 6;
-
-        for (j = 0; j < length; j++)
-        {
-            password[j] = rand() % 94 + 33;
-        }
-
-        password[length] = '\0';
-        printf("%s\n", password);
-    }
-
-    printf("Tada! Congrats\n");
-    return 0;
+int i, length, j;
+char password[12];
+srand(time(NULL));
+for (i = 0; i < 10; i++)
+{
+length = rand() % 7 + 6;
+for (j = 0; j < length; j++)
+{
+password[j] = rand() % 94 + 33;
+}
+password[length] = '\0';
+printf("%s\n", password);
+}
+printf("Tada! Congrats");
+return (0);
 }
