95d0d855ebd37ed0631734658c99f865cb028d18
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-07 22:26:05 +0000

    Task1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _memcpy - Copies memory area
+ * _memcpy - Copies n bytes from memory area src to memory area dest.
  *
  *@dest: memory area
  *@src: memory area
  *@n: number of time
  *
+ * Return: A pointer to the destination memory area (dest).
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
