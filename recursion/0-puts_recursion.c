2847b54b50f4e25005b018a708d91b0cb23718e9
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-09 18:02:03 +0000

    Task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts_recursion - print a string
+ *
+ *@s: the actual character
+ *
+ */
+void _puts_recursion(char *s)
+{
+while (s != '\n')
+{
+_putchar('s');
+s++;
+}
+_putchar('\n');
+}
