cbe594cdd6ade6843b596d33284f69deca919b4f
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-09 17:25:44 +0000

    Task7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,7 +7,8 @@
  *@size: The size of rows or columns
  *
  */
-void print_diagsums(int *a, int size) {
+void print_diagsums(int *a, int size)
+{
 int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++) {
 sum1 += *(a + i * size + i);
