86e0c8abdf4a6577c249283f3352ac5879344929
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:04:43 +0000

    Task1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 int i;
-printf("Number of arguments: %d\n", argc - 1);
+printf("%d\n", argc - 1);
 for (i = 1; i < argc; i++)
 {
 printf("Argument %d: %s\n", i, argv[i]);
