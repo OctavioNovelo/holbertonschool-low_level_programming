5bed4287e9547fb11d9f6e548ce9e3093df7a6a2
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:39:01 +0000

    Task1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-int i;
-printf("%d\n", argc - 1);
-for (i = 1; i < argc; i++)
-{
-printf("%d: %s\n", i, argv[i]);
-}
-return (0);
+int main(int argc, char *argv[]) {
+    printf("%d\n", argc - 1);
+    return 0;
 }
