2e60fab5efbe783057c40354cb1804b5d13372ce
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:21:01 +0000

    Task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - Entry point of the program
  * @argc: The number of command-line arguments
@@ -8,19 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-int num1;
-int num2;
-int result;
+int num1, num2, result;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-if (sscanf(argv[1], "%d", &num1) != 1 || sscanf(argv[2], "%d", &num2) != 1)
-{
-printf("Error\n");
-return (1);
-}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 result = num1 * num2;
 printf("%d\n", result);
 return (0);
