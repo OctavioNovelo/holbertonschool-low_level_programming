9d493da6cb7d4f222bf2fcd65feb48988a32c56a
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:33:41 +0000

    Task5

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -10,18 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
+int cents;
+int coins[];
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-int cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 if (cents < 0)
 {
 printf("0\n");
 return (0);
 }
-int coins[] = {25, 10, 5, 2, 1};
+coins[] = {25, 10, 5, 2, 1};
 int num_coins = sizeof(coins) / sizeof(coins[0]);
 int min_coins = 0;
 int i;
