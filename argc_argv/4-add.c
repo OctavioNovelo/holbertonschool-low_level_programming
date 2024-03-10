f6bd017e5f6d23326a08565db637b459c9fee4bd
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:26:40 +0000

    Task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if there's an error
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (int i = 1; i < argc; i++)
+{
+char *endptr;
+long num = strtol(argv[i], &endptr, 10);
+if (*endptr != '\0' || num < 0)
+{
+printf("Error\n");
+return (1);
+}
+sum += (int)num;
+}
+printf("%d\n", sum);
+return (0);
+}
