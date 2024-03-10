0196977eb4a6cce45c1eea765d4c656c0c9a2462
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 02:42:11 +0000

    Task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 if (argc >= 1)
