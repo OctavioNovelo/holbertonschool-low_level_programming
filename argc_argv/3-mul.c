543f7acf67b1c87aad19ce7b48b0295d0aaeae85
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-10 03:17:03 +0000

    Task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,18 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
+int num1;
+int num2;
+int result;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int num1, num2;
 if (sscanf(argv[1], "%d", &num1) != 1 || sscanf(argv[2], "%d", &num2) != 1)
 {
 printf("Error\n");
 return (1);
 }
-int result = num1 * num2;
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
 }
