4bd38e41b44d3a995d6dc9ec8b88377474e408aa
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-16 20:25:07 +0000

    Task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *new_dog - creates a new dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+return (NULL);
+}
