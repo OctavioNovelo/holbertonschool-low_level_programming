5fb6c54f29a42a6b5253ed79037ca5d721a2c15e
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-22 01:26:32 +0000

    Task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
  *print_name - prints a name.
- *
+ *@name: The name that going to print
+ *@f: A pointer to a function
  */
 void print_name(char *name, void (*f)(char *))
 {
