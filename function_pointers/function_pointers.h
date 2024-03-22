67f9902b277b22c99cbd8002d03765e6b2025940
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-22 00:29:38 +0000

    Task0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 
-#endif /* FUNCTION_POINTERS */
+#endif /* FUNCTION_POINTERS_H */
