7aa9ed4238babe1bb1db75641656b8f4be9ac7cd
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-22 17:46:24 +0000

    Task3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*op)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op = get_op_func(argv[2]);
+if (op == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+result = op(num1, num2);
+printf("%d\n", result);
+return (0);
+}
