7445c6d97a6c720c08b33d68290c07be6c55e814
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-23 18:07:37 +0000

    Task1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,7 +10,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 va_start(args, n);
-for (unsigned int i = 0; i < n; ++i)
+unsigned int i;
+for (i = 0; i < n; ++i)
 {
 printf("%d", va_arg(args, int));
 if (separator != NULL && i != n - 1)
